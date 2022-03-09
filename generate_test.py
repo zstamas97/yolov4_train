38055f9731b5232cbb7c871edd0611792dc4ae9b
zstamas97 <47640134+zstamas97@users.noreply.github.com>
2022-03-09 10:57:23 +0100

    Update generate_test.py

--- generate_test.py
+++ generate_test.py
@@ -4,7 +4,7 @@ image_files = []
 os.chdir(os.path.join("data", "test"))
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        image_files.append("/data/test/" + filename)
+        image_files.append("data/test/" + filename)
 os.chdir("..")
 with open("test.txt", "w") as outfile:
     for image in image_files:
